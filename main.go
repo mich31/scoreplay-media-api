93c73282804687f9e30c86ca4985f2a7cabe63e4
Michel Edjoa <michelaxel@hotmail.com>
2024-11-12 23:20:34 +0000

    setup logger + healthcheck middleware

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/healthcheck"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	app := fiber.New(fiber.Config{
 		AppName: "ScorePlay Media API v0.1",
 	})
+
+	app.Use(logger.New())
+	app.Use(healthcheck.New())
 
 	app.Listen(":3000")
 }
