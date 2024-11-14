f8e35fc61a64f890fe1f28e2b36597d45a29e2f5
Michel Edjoa <michelaxel@hotmail.com>
2024-11-14 15:52:22 +0000

    update tests for tag routes

--- controllers/tag_test.go
+++ controllers/tag_test.go
@@ -1,13 +1,37 @@
 package controllers
 
 import (
+	"bytes"
 	"io"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/mich31/scoreplay-media-api/models"
+	"github.com/mich31/scoreplay-media-api/services"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
+
+type mockTagRepository struct {
+	mock.Mock
+}
+
+func (m *mockTagRepository) Create(tag *models.Tag) (uint, error) {
+	return 1, nil
+}
+
+func (m *mockTagRepository) Find() ([]*models.Tag, error) {
+	return []*models.Tag{}, nil
+}
+
+func (m *mockTagRepository) FindByName(name string) ([]*models.Tag, error) {
+	return []*models.Tag{}, nil
+}
+
+func (m *mockTagRepository) Delete(id string) error {
+	return nil
+}
 
 func TestTagRoutes(t *testing.T) {
 	tests := []struct {
@@ -18,18 +42,25 @@ func TestTagRoutes(t *testing.T) {
 		expectedCode int
 	}{
 		{
-			description:  "Get all tags should return HTTP status 501",
+			description:  "Get all tags should return HTTP status 200",
 			route:        "/api/tags",
 			method:       "GET",
 			body:         nil,
-			expectedCode: 501,
+			expectedCode: 200,
 		},
 		{
-			description:  "Create tag should return HTTP status 501",
+			description:  "Get all tags with a query parameter should return HTTP status 200",
+			route:        "/api/tags?name=zidane",
+			method:       "GET",
+			body:         nil,
+			expectedCode: 200,
+		},
+		{
+			description:  "Create tag should return HTTP status 201",
 			route:        "/api/tags",
 			method:       "POST",
-			body:         nil,
-			expectedCode: 501,
+			body:         bytes.NewBuffer([]byte(`{"name":"Zidane"}`)),
+			expectedCode: 201,
 		},
 		{
 			description:  "Update tag should return HTTP status 501",
@@ -43,19 +74,23 @@ func TestTagRoutes(t *testing.T) {
 			route:        "/api/tags/1",
 			method:       "DELETE",
 			body:         nil,
-			expectedCode: 501,
+			expectedCode: 200,
 		},
 	}
+
+	tagRepository := &mockTagRepository{}
+	tagService := services.NewTagService(tagRepository)
+	tagController := NewTagController(*tagService)
 
 	app := fiber.New()
 	api := app.Group("/api")
 
 	// routes
 	api.Route("tags", func(router fiber.Router) {
-		router.Get("/", GetTags)
-		router.Post("/", CreateTag)
-		router.Patch("/:id", UpdateTag)
-		router.Delete("/:id", DeleteTag)
+		router.Get("/", tagController.GetTags)
+		router.Post("/", tagController.CreateTag)
+		router.Patch("/:id", tagController.UpdateTag)
+		router.Delete("/:id", tagController.DeleteTag)
 	})
 
 	// Iterate through test single test cases
