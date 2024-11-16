4d5fa580bdc6997b16c29ab5eab169810e7c0aef
Michel Edjoa <michelaxel@hotmail.com>
2024-11-16 00:13:37 +0000

    declare indexes

--- models/media.go
+++ models/media.go
@@ -9,7 +9,7 @@ import (
 // Media model
 type Media struct {
 	ID          uint   `json:"id" gorm:"primaryKey"`
-	Name        string `json:"name" gorm:"not null"`
+	Name        string `json:"name" gorm:"not null;index:idx_media_name"`
 	Description string `json:"description" gorm:"size:100"`
 	FileUrl     string `json:"fileUrl" gorm:"not null"`
 	FileSize    int64
@@ -20,8 +20,8 @@ type Media struct {
 
 // MediaTag model (junction table)
 type MediaTag struct {
-	MediaID   uint   `gorm:"primaryKey;column:media_id"`
-	TagID     uint   `gorm:"primaryKey;column:tag_id"`
+	MediaID   uint   `gorm:"primaryKey;column:media_id;index:idx_media_tags_media_id"`
+	TagID     uint   `gorm:"primaryKey;column:tag_id;index:idx_media_tags_media_id"`
 	Media     *Media `gorm:"foreignKey:MediaID"`
 	Tag       *Tag   `gorm:"foreignKey:TagID"`
 	CreatedAt time.Time
--- models/tag.go
+++ models/tag.go
@@ -5,7 +5,7 @@ import "time"
 // Tag model
 type Tag struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
-	Name      string    `json:"name" gorm:"unique;not null"`
+	Name      string    `json:"name" gorm:"unique;not null;index:idx_tags_name"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
 }
