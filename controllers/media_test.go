9365bb4a7d4c50548730548a077cdac293bbc128
Michel Edjoa <michelaxel@hotmail.com>
2024-11-18 05:23:56 +0100

    refactor tests + improve mocks

--- controllers/media_test.go
+++ controllers/media_test.go
@@ -1,12 +1,18 @@
 package controllers
 
 import (
+	"bytes"
+	"context"
+	"errors"
 	"io"
+	"mime/multipart"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/mich31/scoreplay-media-api/models"
+	"github.com/mich31/scoreplay-media-api/repositories"
 	"github.com/mich31/scoreplay-media-api/services"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -16,60 +22,268 @@ type mockMediaRepository struct {
 	mock.Mock
 }
 
-func (m *mockMediaRepository) Create(media *models.Media, tags []string) (uint, error) {
-	return 1, nil
+type mockStorageService struct {
+	mock.Mock
 }
 
-func (m *mockMediaRepository) FindByTag(tag string) ([]models.MediaWithTagNames, error) {
-	return []models.MediaWithTagNames{}, nil
+func (r *mockMediaRepository) Create(media *models.Media, tagIDs []uint) (uint, error) {
+	args := r.Called(media, tagIDs)
+	return args.Get(0).(uint), args.Error(1)
 }
 
-func TestMediaRoutes(t *testing.T) {
+func (r *mockMediaRepository) FindByTag(tag string) ([]models.MediaWithTagNames, error) {
+	args := r.Called(tag)
+	return args.Get(0).([]models.MediaWithTagNames), args.Error(1)
+}
+
+func (s *mockStorageService) CreateBucket(ctx context.Context, bucketName string) error {
+	args := s.Called(ctx)
+	return args.Error(1)
+}
+
+func (s *mockStorageService) UploadObject(ctx context.Context, fileHeader *multipart.FileHeader) (string, error) {
+	args := s.Called(ctx, fileHeader)
+	return args.Get(0).(string), args.Error(1)
+}
+
+func TestGetMedias(t *testing.T) {
 	tests := []struct {
-		description  string
-		route        string
-		method       string
-		body         io.Reader
-		expectedCode int
+		description          string
+		tag                  string
+		mockReturn           []models.MediaWithTagNames
+		mockError            error
+		expectedStatusCode   int
+		expectedBodyResponse string
 	}{
 		{
-			description:  "Get all medias should return HTTP status 501",
-			route:        "/api/medias",
-			method:       "GET",
-			body:         nil,
-			expectedCode: 501,
+			description: "Get medias should return a list of medias associated to a given tag and HTTP status code 200",
+			tag:         "hernandez",
+			mockReturn: []models.MediaWithTagNames{
+				{
+					ID:          1,
+					Name:        "lucas_hernandez",
+					Description: "Lucas Hernandez",
+					FileUrl:     "http://localhost:9000/medias/611e175c-c0bc-488e-b4b7-f5d005e4fa5b.png",
+					TagNames:    []string{"hernandez", "football", "france"},
+				},
+			},
+			mockError:          nil,
+			expectedStatusCode: 200,
+			expectedBodyResponse: `{
+				"success":true,
+				"message":"",
+				"data":[
+					{"id":1,"name":"lucas_hernandez", "description":"Lucas Hernandez", "fileUrl":"http://localhost:9000/medias/611e175c-c0bc-488e-b4b7-f5d005e4fa5b.png", "tagNames": ["hernandez", "football", "france"] }
+				]}`,
 		},
 		{
-			description:  "Create media should return HTTP status 501",
-			route:        "/api/medias",
-			method:       "POST",
-			body:         nil,
-			expectedCode: 501,
+			description:        "Get medias should return an empty list for an unexisting tag and HTTP status code 404",
+			tag:                "unexisting_tag",
+			mockReturn:         []models.MediaWithTagNames{},
+			mockError:          nil,
+			expectedStatusCode: 404,
+			expectedBodyResponse: `{
+				"success":true,
+				"message":"",
+				"data":[]}`,
+		},
+		{
+			description:        "Get medias should return HTTP status code 500 if an unexpected error occurs",
+			tag:                "unexisting_tag",
+			mockReturn:         nil,
+			mockError:          errors.New("database unreachable"),
+			expectedStatusCode: 500,
+			expectedBodyResponse: `{
+				"success":false,
+				"message":"database unreachable",
+				"data":null}`,
 		},
 	}
 
-	mediaRepository := &mockMediaRepository{}
-	tagRepository := &mockTagRepository{}
-	mediaService := services.NewMediaService(mediaRepository, tagRepository)
-	mediaController := NewMediaController(*mediaService)
+	for _, tt := range tests {
+		t.Run(tt.description, func(t *testing.T) {
+			app := fiber.New()
+			api := app.Group("/api")
 
-	app := fiber.New()
-	api := app.Group("/api")
+			mockMediaRepository := new(mockMediaRepository)
+			mockMediaRepository.On("FindByTag", tt.tag).Return(tt.mockReturn, tt.mockError)
+			mockTagRepository := new(mockTagRepository)
+			mockStorageService := new(mockStorageService)
+			mediaService := services.NewMediaService(mockMediaRepository, mockTagRepository, mockStorageService)
+			mediaController := NewMediaController(*mediaService)
 
-	// routes
-	api.Route("medias", func(router fiber.Router) {
-		router.Get("/", mediaController.GetMedias)
-		router.Post("/", mediaController.CreateMedia)
-	})
+			// routes
+			api.Route("medias", func(router fiber.Router) {
+				router.Get("/", mediaController.GetMedias)
+			})
 
-	// Iterate through test single test cases
-	for _, test := range tests {
-		req := httptest.NewRequest(test.method, test.route, test.body)
-		req.Header.Set("Content-Type", "application/json")
+			req := httptest.NewRequest("GET", "/api/medias?tag="+tt.tag, nil)
+			resp, _ := app.Test(req)
 
-		resp, _ := app.Test(req, 1)
+			assert.Equal(t, tt.expectedStatusCode, resp.StatusCode)
+			body, _ := io.ReadAll(resp.Body)
+			assert.JSONEq(t, tt.expectedBodyResponse, string(body))
+			mockMediaRepository.AssertExpectations(t)
+		})
+	}
+}
 
-		// Verify, if the status code is as expected.
-		assert.Equalf(t, test.expectedCode, resp.StatusCode, test.description)
+func TestCreateMedia(t *testing.T) {
+	tests := []struct {
+		description          string
+		setupRequest         func() (*http.Request, error)
+		mockTagIDs           []uint
+		mockFileUrl          string
+		mockId               uint
+		mockRepositoryError  error
+		mockStorageError     error
+		expectedStatusCode   int
+		expectedBodyResponse string
+	}{
+		{
+			description: "Create media should return HTTP status code 200",
+			setupRequest: func() (*http.Request, error) {
+				body := &bytes.Buffer{}
+				writer := multipart.NewWriter(body)
+				part, _ := writer.CreateFormFile("file", "baseball.png")
+				part.Write([]byte("baseball game"))
+				writer.WriteField("name", "baseball")
+				writer.WriteField("tags", "[1,2]")
+				writer.Close()
+
+				req := httptest.NewRequest("POST", "/api/medias", body)
+				req.Header.Set("Content-Type", writer.FormDataContentType())
+				return req, nil
+			},
+			mockFileUrl:          "http://localhost:9000/medias/611e175c-c0bc-488e-b4b7-f5d005e4fa5b.png",
+			mockTagIDs:           []uint{1, 2},
+			mockId:               1,
+			mockRepositoryError:  nil,
+			mockStorageError:     nil,
+			expectedStatusCode:   201,
+			expectedBodyResponse: `{"success":true,"message":"File uploaded"}`,
+		},
+		{
+			description: "Create media should return HTTP status code 400 if tags parameter is invalid",
+			setupRequest: func() (*http.Request, error) {
+				body := &bytes.Buffer{}
+				writer := multipart.NewWriter(body)
+				part, _ := writer.CreateFormFile("file", "baseball.png")
+				part.Write([]byte("baseball game"))
+				writer.WriteField("name", "test video")
+				writer.WriteField("tags", "18")
+				writer.Close()
+
+				req := httptest.NewRequest("POST", "/api/medias", body)
+				req.Header.Set("Content-Type", writer.FormDataContentType())
+				return req, nil
+			},
+			mockFileUrl:          "",
+			mockTagIDs:           nil,
+			mockId:               0,
+			mockRepositoryError:  nil,
+			mockStorageError:     nil,
+			expectedStatusCode:   400,
+			expectedBodyResponse: `{"success":false,"message":"Invalid tags format: json: cannot unmarshal number into Go value of type []uint"}`,
+		},
+		{
+			description: "Create media should return HTTP status code 400 if tags parameter is invalid",
+			setupRequest: func() (*http.Request, error) {
+				body := &bytes.Buffer{}
+				writer := multipart.NewWriter(body)
+				writer.WriteField("name", "rugby")
+				writer.WriteField("tags", "[1,2]")
+				writer.Close()
+
+				req := httptest.NewRequest("POST", "/api/medias", body)
+				req.Header.Set("Content-Type", writer.FormDataContentType())
+				return req, nil
+			},
+			mockFileUrl:          "",
+			mockTagIDs:           nil,
+			mockId:               0,
+			mockRepositoryError:  nil,
+			mockStorageError:     nil,
+			expectedStatusCode:   400,
+			expectedBodyResponse: `{"success":false,"message":"Missing file to upload"}`,
+		},
+		{
+			description: "Create media should return HTTP status code 400 when the media already exists",
+			setupRequest: func() (*http.Request, error) {
+				body := &bytes.Buffer{}
+				writer := multipart.NewWriter(body)
+				part, _ := writer.CreateFormFile("file", "baseball.png")
+				part.Write([]byte("baseball game"))
+				writer.WriteField("name", "baseball")
+				writer.WriteField("tags", "[1,2]")
+				writer.Close()
+
+				req := httptest.NewRequest("POST", "/api/medias", body)
+				req.Header.Set("Content-Type", writer.FormDataContentType())
+				return req, nil
+			},
+			mockFileUrl:          "http://localhost:9000/medias/611e175c-c0bc-488e-b4b7-f5d005e4fa5b.png",
+			mockTagIDs:           []uint{1, 2},
+			mockId:               1,
+			mockRepositoryError:  repositories.ErrMediaExists,
+			mockStorageError:     nil,
+			expectedStatusCode:   400,
+			expectedBodyResponse: `{"success":false,"message":"Failed to create media: a media with the same name already exists"}`,
+		},
+		{
+			description: "Create media should return HTTP status code 500 if an unexpected error occurs",
+			setupRequest: func() (*http.Request, error) {
+				body := &bytes.Buffer{}
+				writer := multipart.NewWriter(body)
+				part, _ := writer.CreateFormFile("file", "baseball.png")
+				part.Write([]byte("baseball game"))
+				writer.WriteField("name", "baseball")
+				writer.WriteField("tags", "[1,2]")
+				writer.Close()
+
+				req := httptest.NewRequest("POST", "/api/medias", body)
+				req.Header.Set("Content-Type", writer.FormDataContentType())
+				return req, nil
+			},
+			mockFileUrl:          "http://localhost:9000/medias/611e175c-c0bc-488e-b4b7-f5d005e4fa5b.png",
+			mockTagIDs:           []uint{1, 2},
+			mockId:               1,
+			mockRepositoryError:  repositories.ErrMediaCreation,
+			mockStorageError:     nil,
+			expectedStatusCode:   500,
+			expectedBodyResponse: `{"success":false,"message":"Failed to create media: failed to create media record"}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.description, func(t *testing.T) {
+			app := fiber.New()
+			api := app.Group("/api")
+
+			mockMediaRepository := new(mockMediaRepository)
+			mockMediaRepository.On("Create", mock.AnythingOfType("*models.Media"), tt.mockTagIDs).Return(tt.mockId, tt.mockRepositoryError)
+			mockTagRepository := new(mockTagRepository)
+			mockStorageService := new(mockStorageService)
+			mockStorageService.On(
+				"UploadObject",
+				mock.Anything,
+				mock.AnythingOfType("*multipart.FileHeader")).
+				Return(tt.mockFileUrl, tt.mockStorageError)
+			mediaService := services.NewMediaService(mockMediaRepository, mockTagRepository, mockStorageService)
+			mediaController := NewMediaController(*mediaService)
+
+			// routes
+			api.Route("medias", func(router fiber.Router) {
+				router.Post("/", mediaController.CreateMedia)
+			})
+
+			req, err := tt.setupRequest()
+			assert.NoError(t, err)
+			resp, _ := app.Test(req)
+
+			assert.Equal(t, tt.expectedStatusCode, resp.StatusCode)
+			body, _ := io.ReadAll(resp.Body)
+			assert.JSONEq(t, tt.expectedBodyResponse, string(body))
+		})
 	}
 }
--- controllers/tag_test.go
+++ controllers/tag_test.go
@@ -1,9 +1,10 @@
 package controllers
 
 import (
-	"bytes"
+	"errors"
 	"io"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/gofiber/fiber/v2"
@@ -18,81 +19,198 @@ type mockTagRepository struct {
 }
 
 func (m *mockTagRepository) Create(tag *models.Tag) (uint, error) {
-	return 1, nil
+	args := m.Called(tag)
+	return args.Get(0).(uint), args.Error(1)
 }
 
 func (m *mockTagRepository) Find() ([]*models.Tag, error) {
-	return []*models.Tag{}, nil
+	args := m.Called()
+	return args.Get(0).([]*models.Tag), args.Error(1)
 }
 
 func (m *mockTagRepository) FindByName(name string) ([]*models.Tag, error) {
-	return []*models.Tag{}, nil
+	args := m.Called(name)
+	return args.Get(0).([]*models.Tag), args.Error(1)
 }
 
 func (m *mockTagRepository) Delete(id string) error {
-	return nil
+	args := m.Called(id)
+	return args.Error(0)
 }
 
-func TestTagRoutes(t *testing.T) {
+func TestGetTags(t *testing.T) {
 	tests := []struct {
-		description  string
-		route        string
-		method       string
-		body         io.Reader
-		expectedCode int
+		description          string
+		tagName              string
+		mockTags             []*models.Tag
+		mockError            error
+		expectedStatusCode   int
+		expectedBodyResponse string
 	}{
 		{
-			description:  "Get all tags should return HTTP status 200",
-			route:        "/api/tags",
-			method:       "GET",
-			body:         nil,
-			expectedCode: 200,
+			description: "Get all tags should return a list of tags and HTTP status code 200",
+			tagName:     "",
+			mockTags: []*models.Tag{
+				{ID: 1, Name: "Zidane"},
+				{ID: 2, Name: "Brady"},
+				{ID: 3, Name: "Hamilton"},
+			},
+			mockError:          nil,
+			expectedStatusCode: 200,
+			expectedBodyResponse: `{
+				"success":true,
+				"message":"",
+				"data":[
+					{"id":1,"name":"Zidane", "createdAt":"0001-01-01T00:00:00Z", "updatedAt":"0001-01-01T00:00:00Z"},
+					{"id":2,"name":"Brady", "createdAt":"0001-01-01T00:00:00Z", "updatedAt":"0001-01-01T00:00:00Z"},
+					{"id":3,"name":"Hamilton", "createdAt":"0001-01-01T00:00:00Z", "updatedAt":"0001-01-01T00:00:00Z"}
+				]}`,
 		},
 		{
-			description:  "Get all tags with a query parameter should return HTTP status 200",
-			route:        "/api/tags?name=zidane",
-			method:       "GET",
-			body:         nil,
-			expectedCode: 200,
+			description: "Get all tags with a query parameter should return a list of tags and HTTP status 200",
+			tagName:     "paul",
+			mockTags: []*models.Tag{
+				{ID: 4, Name: "Paul Scholes"},
+				{ID: 7, Name: "Paul Pogba"},
+			},
+			mockError:          nil,
+			expectedStatusCode: 200,
+			expectedBodyResponse: `{
+				"success":true,
+				"message":"",
+				"data":[
+					{"id":4,"name":"Paul Scholes", "createdAt":"0001-01-01T00:00:00Z", "updatedAt":"0001-01-01T00:00:00Z"},
+					{"id":7,"name":"Paul Pogba", "createdAt":"0001-01-01T00:00:00Z", "updatedAt":"0001-01-01T00:00:00Z"}
+				]}`,
 		},
 		{
-			description:  "Create tag should return HTTP status 201",
-			route:        "/api/tags",
-			method:       "POST",
-			body:         bytes.NewBuffer([]byte(`{"name":"Zidane"}`)),
-			expectedCode: 201,
+			description:        "Get all tags with an unexisting tag name as query parameter should return an empty list of tags and HTTP status 200",
+			tagName:            "john",
+			mockTags:           []*models.Tag{},
+			mockError:          nil,
+			expectedStatusCode: 200,
+			expectedBodyResponse: `{
+				"success":true,
+				"message":"",
+				"data":[]}`,
 		},
 		{
-			description:  "Delete tag should return HTTP status 501",
-			route:        "/api/tags/1",
-			method:       "DELETE",
-			body:         nil,
-			expectedCode: 200,
+			description:        "Get all tags should return HTTP status 500 if an unexpected error occurs",
+			tagName:            "",
+			mockTags:           nil,
+			mockError:          errors.New("database unreachable"),
+			expectedStatusCode: 500,
+			expectedBodyResponse: `{
+				"success":false,
+				"message":"database unreachable",
+				"data":null}`,
 		},
 	}
 
-	tagRepository := &mockTagRepository{}
-	tagService := services.NewTagService(tagRepository)
-	tagController := NewTagController(*tagService)
+	for _, tt := range tests {
+		t.Run(tt.description, func(t *testing.T) {
+			app := fiber.New()
+			api := app.Group("/api")
 
-	app := fiber.New()
-	api := app.Group("/api")
+			mockTagRepository := new(mockTagRepository)
+			if tt.mockError != nil {
+				mockTagRepository.On("Find").Return(tt.mockTags, tt.mockError)
+			} else if tt.tagName != "" {
+				mockTagRepository.On("FindByName", tt.tagName).Return(tt.mockTags, tt.mockError)
+			} else {
+				mockTagRepository.On("Find").Return(tt.mockTags, tt.mockError)
+			}
+			tagService := services.NewTagService(mockTagRepository)
+			tagController := NewTagController(*tagService)
 
-	// routes
-	api.Route("tags", func(router fiber.Router) {
-		router.Get("/", tagController.GetTags)
-		router.Post("/", tagController.CreateTag)
-		router.Delete("/:id", tagController.DeleteTag)
-	})
+			// routes
+			api.Route("tags", func(router fiber.Router) {
+				router.Get("/", tagController.GetTags)
+			})
 
-	// Iterate through test single test cases
-	for _, test := range tests {
-		req := httptest.NewRequest(test.method, test.route, test.body)
-		req.Header.Set("Content-Type", "application/json")
+			req := httptest.NewRequest("GET", "/api/tags?name="+tt.tagName, nil)
+			resp, _ := app.Test(req)
 
-		resp, _ := app.Test(req, 1)
+			assert.Equal(t, tt.expectedStatusCode, resp.StatusCode)
+			body, _ := io.ReadAll(resp.Body)
+			assert.JSONEq(t, tt.expectedBodyResponse, string(body))
+			mockTagRepository.AssertExpectations(t)
+		})
+	}
+}
 
-		// Verify, if the status code is as expected.
-		assert.Equalf(t, test.expectedCode, resp.StatusCode, test.description)
+func TestCreateTag(t *testing.T) {
+	tests := []struct {
+		description          string
+		body                 string
+		mockId               uint
+		mockError            error
+		expectedStatusCode   int
+		expectedBodyResponse string
+	}{
+		{
+			description:        "Create tag should return id of the tag created and HTTP status code 201",
+			body:               `{ "name":"nba" }`,
+			mockId:             1,
+			mockError:          nil,
+			expectedStatusCode: 201,
+			expectedBodyResponse: `{
+				"success":true,
+				"message":"",
+				"id": 1
+				}`,
+		},
+		{
+			description:        "Create tag should return an error and HTTP status code 400",
+			body:               `"nba"`,
+			mockId:             1,
+			mockError:          nil,
+			expectedStatusCode: 400,
+			expectedBodyResponse: `{
+				"success":false,
+				"message":"json: cannot unmarshal string into Go value of type models.Tag",
+				"id": 0
+				}`,
+		},
+		{
+			description:        "Create tag should return HTTP status code 500 if an unexpected error occurs",
+			body:               `{ "name":"nba" }`,
+			mockId:             0,
+			mockError:          errors.New("database unreachable"),
+			expectedStatusCode: 500,
+			expectedBodyResponse: `{
+				"success":false,
+				"message":"database unreachable",
+				"id": 0
+			}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.description, func(t *testing.T) {
+			app := fiber.New()
+			api := app.Group("/api")
+
+			mockTagRepository := new(mockTagRepository)
+			mockTagRepository.On("Create", mock.AnythingOfType("*models.Tag")).Return(tt.mockId, tt.mockError)
+			tagService := services.NewTagService(mockTagRepository)
+			tagController := NewTagController(*tagService)
+
+			// routes
+			api.Route("tags", func(router fiber.Router) {
+				router.Post("/", tagController.CreateTag)
+			})
+
+			req := httptest.NewRequest("POST", "/api/tags", strings.NewReader(tt.body))
+			req.Header.Set("Content-Type", "application/json")
+			resp, _ := app.Test(req)
+
+			assert.Equal(t, tt.expectedStatusCode, resp.StatusCode)
+			body, _ := io.ReadAll(resp.Body)
+			assert.JSONEq(t, tt.expectedBodyResponse, string(body))
+			if tt.expectedStatusCode != 400 {
+				mockTagRepository.AssertExpectations(t)
+			}
+		})
 	}
 }
